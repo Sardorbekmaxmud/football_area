22e79ccb5b6261aab5f078c4715dba2e31252f98
almasbek2606 <atabekovalmasbek@gmail.com>
2023-07-28 10:01:59 +0500

    add

--- football_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FootballAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'football_app'
--- football_app/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import StadiumModel
-
-
-class StadiumSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StadiumModel
-        fields = ('id', 'address', 'contact', 'images', 'price', 'started_at', 'end_at', )
